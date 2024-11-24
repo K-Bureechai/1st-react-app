ca067203d3c56993307add2ef517303abe51f36c
May <k.bureechai@gmail.com>
2024-11-24 17:56:30 +0700

    push to git hub

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`It is ${response.data.main.temp} degree in ${props.city}`);
+  }
+
+  let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <ClipLoader
+      color="#000000"
+      size={150}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+  );
+}
